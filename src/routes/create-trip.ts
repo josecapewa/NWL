2e3f7233f69f8b640e6f7b9c02d43a804882a766
José Capewa <capewajose@gmail.com>
2024-07-14 22:58:22 +0000

    chore: Add fastify-type-provider-zod as a dependency and implement trip creation route

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,8 +1,27 @@
 import { FastifyInstance } from "fastify";
-import zod from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
 
 export async function createTrip(app: FastifyInstance){
-    app.post("/trips", async () => {
-        return "Hello world";
+    app.withTypeProvider<ZodTypeProvider>().post("/trips", {
+        schema: {
+            body: z.object({
+                destination: z.string().min(4),
+                starts_at: z.coerce.date(),
+                ends_at: z.coerce.date(),
+            })
+        }
+        }, async (request) => {
+        const { destination, starts_at, ends_at } = request.body;
+
+        const trip = await prisma.trip.create({
+            data: {
+                destination,
+                starts_at,
+                ends_at,
+            }
+        });
+        return {tripid: trip.id};
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
 import { createTrip } from "./routes/create-trip";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 
