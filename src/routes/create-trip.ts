acd87c54306e57d4af669025516fa706326fc031
José Capewa <capewajose@gmail.com>
2024-07-14 23:54:42 +0000

    feat: Add nodemailer for sending trip creation confirmation email

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -2,6 +2,9 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
 
 export async function createTrip(app: FastifyInstance){
     app.withTypeProvider<ZodTypeProvider>().post("/trips", {
@@ -10,10 +13,20 @@ export async function createTrip(app: FastifyInstance){
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.string().email(),
             })
         }
         }, async (request) => {
-        const { destination, starts_at, ends_at } = request.body;
+        const { destination, starts_at, ends_at, owner_name, owner_email } = request.body;
+
+        if(dayjs(starts_at).isBefore(new Date())){
+            throw new Error("Invalid trip start date.");
+        }
+
+        if(dayjs(ends_at).isBefore(dayjs(starts_at))){
+            throw new Error("Invalid trip end date.");
+        }
 
         const trip = await prisma.trip.create({
             data: {
@@ -22,6 +35,23 @@ export async function createTrip(app: FastifyInstance){
                 ends_at,
             }
         });
+
+        const mail = await getMailClient();
+
+        const message = await mail.sendMail({
+            from: {
+                name: "Trip Planner",
+                address: "oi@jose.com"
+            },
+            to: {
+                name: owner_name,
+                address: owner_email
+            },
+            subject: "Trip created",
+            text: `Hello ${owner_name}, your trip to ${destination} has been created successfully.`
+        });
+
+        console.log(nodemailer.getTestMessageUrl(message));
         return {tripid: trip.id};
     })
 }
